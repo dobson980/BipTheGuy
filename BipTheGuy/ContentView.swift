c18753a91dfa6ea9db96f9174ecdd4c9f6c38b8a
Tom Dobson <dobson980@gmail.com>
2024-07-13 23:20:16 -0700

    final app

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -6,17 +6,65 @@
 //
 
 import SwiftUI
+import AVFAudio
+import PhotosUI
 
 struct ContentView: View {
+    @State private var audioPlayer: AVAudioPlayer?
+    @State private var animateImage = true
+    @State private var selectedPhoto: PhotosPickerItem?
+    @State private var bipImage = Image("clown")
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            
+            bipImage
+                .resizable()
+                .scaledToFit()
+                .scaleEffect(animateImage ? 1.0 : 0.9)
+                .onTapGesture {
+                    playSound(soundName: "punch")
+                    animateImage = false
+                    withAnimation(.spring(response: 0.3, dampingFraction: 0.3)) {
+                        animateImage = true
+                    }
+                }
+            
+            Spacer()
+            
+            PhotosPicker(selection: $selectedPhoto, matching: .images) {
+                Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
+            }
+            .buttonStyle(.borderedProminent)
+            .onChange(of: selectedPhoto) {
+                Task {
+                    if let newImage = try? await selectedPhoto?.loadTransferable(type: Image.self) {
+                        bipImage = newImage
+                    } else {
+                        print("Failed to set new image")
+                    }
+                }
+            }
+
         }
         .padding()
     }
+    
+    func playSound(soundName: String) {
+        guard let soundFile = NSDataAsset(name: soundName) else {
+            print("Could not read file named: \(soundName)")
+            return
+        }
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer?.prepareToPlay()
+            audioPlayer?.play()
+        } catch {
+            print("Error: \(error.localizedDescription) creating audioPlayer")
+        }
+    }
+    
 }
 
 #Preview {
